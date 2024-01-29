1d2b74129051f94dae80d5a498f9f01881e5bc7a
SoundaryaNattuva <soundaryanatts@gmail.com>
2024-01-29 17:55:46 -0500

    create candy and drink divs for vending machine with logic

---
+++ src/Candy.jsx
@@ -0,0 +1,10 @@
+const Candy = (props) => {
+  return ( 
+    <div>{props.candy.name}: 
+      <li>Quantity - {props.candy.quantity}</li>
+      <li>Size - {props.candy.size}</li>
+    </div>
+  );
+}
+
+export default Candy
---
+++ src/VendingMachine.jsx
@@ -0,0 +1,23 @@
+import Candy from './Candy';
+import Drink from './Drink';
+
+const VendingMachine = (props) => {
+  return ( 
+    <div>
+      <h1>Vending Machine</h1>
+      <div> <h2>Candy</h2>
+        <Candy candy={{name: "Bruno", size: "large", quantity: "30"}}  />
+        <Candy candy={{name: "Skittles", size: "small", quantity: "25"}}  />
+        <Candy candy={{name: "Reese's", size: "large", quantity: "35"}}  />
+      </div>
+      <div> <h2>Drinks</h2>
+        <Drink drink={{name: "Sprite", fizz:1}}/>
+        <Drink drink={{name: "Water", fizz:0}}/>
+        <Drink drink={{name: "Orange Juice", fizz:0}}/>
+      </div>
+
+    </div>
+  );
+}
+
+export default VendingMachine;
